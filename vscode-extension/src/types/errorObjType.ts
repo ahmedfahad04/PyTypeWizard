43ca1903ea625ccc4cebb290187d97600333a8b5
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2025-01-05 07:52:26 +0600

    refactor: moved old extension into others folder

--- vscode-extension/src/types/errorObjType.ts
+++
@@ -1,9 +0,0 @@
-export interface ErrorObjectType {
-    file_name: string;
-    display_name: string;
-    rule_id: string;
-    message: string;
-    line_num: number;
-    col_num: number;
-    length: number
-}
