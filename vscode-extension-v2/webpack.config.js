1e344d86d0b6e7d6b530b2442a8f6f78353d8f0b
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2025-01-22 23:04:21 +0600

    feat!: fixed database initializing issue

--- vscode-extension-v2/webpack.config.js
+++ vscode-extension-v2/webpack.config.js
@@ -1,7 +1,7 @@
 //@ts-check
 
 'use strict';
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 //@ts-check
@@ -9,10 +9,10 @@ const path = require('path');
 
 /** @type WebpackConfig */
 const extensionConfig = {
+  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
-  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'dist'),
@@ -21,13 +21,14 @@ const extensionConfig = {
   },
   externals: {
     vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    'vscode-languageclient': 'commonjs vscode-languageclient'
+    'vscode-languageclient': 'commonjs vscode-languageclient',
+    sqlite3: 'commonjs sqlite3', //! sqlite3 is a native module and must be excluded
 
     // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js', '.json', '.node'] //! added .node and .json to support native modules
   },
   module: {
     rules: [
@@ -39,9 +40,23 @@ const extensionConfig = {
             loader: 'ts-loader'
           }
         ]
-      }
+      },
+      {
+        test: /\.node$/,
+        use: 'node-loader', //! Use node-loader for .node files
+      },
     ]
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'node_modules/sqlite3/build/Release/node_sqlite3.node'),
+          to: path.resolve(__dirname, 'dist/node_modules/sqlite3/lib/binding/[name][ext]'),
+        },
+      ],
+    }),
+  ],
   devtool: 'nosources-source-map',
   infrastructureLogging: {
     level: "log", // enables logging required for problem matchers
